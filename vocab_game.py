4ee7364a973616d6fa3a3fa9bdddee04d3c1ae12
406-01-arnachak <54134@yupparaj.ac.th>
2026-08-21 11:05:38 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -97,13 +97,6 @@ ans2 = st.text_input(
     "ข้อ 2: Cats love to eat `f _ s h`. 🐟",
     value=st.session_state.ans2_val,
 )
-
-# อัปเดตค่าล่าสุดเข้าตัวแปร
-st.session_state.ans1_val = ans1
-st.session_state.ans2_val = ans2
-st.session_state.ans3_val = ans3
-st.session_state.ans4_val = ans4
-
 ans3 = st.text_input(
     "ข้อ 3: Who is number 12 in the class `Ple_n_`.",
     value=st.session_state.ans3_val,
@@ -111,7 +104,12 @@ ans3 = st.text_input(
 ans3 = st.text_input(
     "ข้อ 3: Name number 12  `Nont_na_t`. ",
     value=st.session_state.ans3_val,
-  )
+    
+# อัปเดตค่าล่าสุดเข้าตัวแปร
+st.session_state.ans1_val = ans1
+st.session_state.ans2_val = ans2
+st.session_state.ans3_val = ans3
+st.session_state.ans4_val = ans4
 
 # 4. ปุ่มส่งคำตอบ
 if "start" in st.session_state and not st.session_state.get("is_ended", False):
